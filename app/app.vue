4fd0663fbb0df52e8ce4f87611b4f4019e6fec61
The Pirate <thmellocontato@gmail.com>
2026-02-06 12:30:28 -0300

    feat(app): replace Discord invite button with embedded widget
    Replace the static Discord invite button with an embedded Discord widget iframe to provide a more interactive community preview. Also add a dedicated close button for better modal UX.

--- app/app.vue
+++ app/app.vue
@@ -166,27 +166,19 @@
 
   </section>
   <dialog id="open_beta_detona" class="modal" open>
-    <div class="modal-box bg-[url(/DIVUL_OBT.png)] bg-cover aspect-[1080/1350] max-md:p-8 p-16 flex justify-center items-end">
-      <NuxtLink to="https://discord.gg/eUth6mTQzR" target="_blank" class="w-full">
-        <button class="btn btn-secondary gap-3 w-full">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_42_2)">
-              <path
-                d="M20.3001 4.51C18.7216 3.79651 17.058 3.28901 15.3501 3C15.1154 3.41473 14.9038 3.84204 14.7161 4.28C12.8961 4.00844 11.046 4.00844 9.22605 4.28C9.03738 3.844 8.82605 3.41733 8.59205 3C6.88205 3.289 5.21205 3.797 3.63205 4.51C0.502052 9.1 -0.347948 13.57 0.0720517 18.01C1.91205 19.35 3.96205 20.37 6.14205 21.03C6.6354 20.376 7.07041 19.68 7.44205 18.95C6.73205 18.688 6.04205 18.364 5.39205 17.982C5.56405 17.858 5.73138 17.7333 5.89405 17.608C7.79574 18.4912 9.86726 18.9488 11.9641 18.9488C14.0608 18.9488 16.1324 18.4912 18.0341 17.608C18.1987 17.742 18.3661 17.8667 18.5361 17.982C17.8823 18.3644 17.1963 18.689 16.4861 18.952C16.8591 19.678 17.2941 20.372 17.7861 21.022C19.9761 20.37 22.0261 19.352 23.8661 18.012C24.3641 12.902 23.0151 8.462 20.2961 4.512L20.3001 4.51ZM8.00005 15.31C6.82005 15.31 5.84005 14.25 5.84005 12.94C5.84005 11.63 6.78405 10.56 8.00005 10.56C9.21005 10.56 10.1801 11.63 10.1601 12.94C10.1401 14.25 9.20705 15.31 8.00005 15.31ZM15.9701 15.31C14.7801 15.31 13.8101 14.25 13.8101 12.94C13.8101 11.63 14.7541 10.56 15.9701 10.56C17.1901 10.56 18.1501 11.63 18.1301 12.94C18.1101 14.25 17.1791 15.31 15.9701 15.31Z"
-                fill="currentColor" />
-            </g>
-            <defs>
-              <clipPath id="clip0_42_2">
-                <rect width="24" height="24" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-          JOIN / PARTICIPAR
-        </button>
-      </NuxtLink>
+    <div class="relative modal-box aspect-[1080/1350] flex flex-col p-0">
+      <iframe src="https://discord.com/widget?id=1449403936007262443&theme=dark" width="100%" height="100%" class="aspect-[1080/1350]" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
     </div>
     <form method="dialog" class="modal-backdrop">
       <button>close</button>
+    </form>
+    <form method="dialog" class="absolute top-4 right-4">
+      <button class="btn btn-sm btn-circle btn-primary">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18 6L6 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </button>
     </form>
   </dialog>
 </template>
